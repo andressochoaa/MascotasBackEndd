fc03ca181bfbddeafe4c92a0023c0199e557e944
Andres Ochoa <andressochoaa@gmail.com>
2023-06-15 18:56:41 -0500

    Agregar archivos de proyecto.

---
+++ MascotasBackEnd/Models/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MascotasBackEnd.Models
+{
+    public class AppDbContext : DbContext
+    {
+
+    }
+}
---
+++ MascotasBackEnd/Models/Mascota.cs
@@ -0,0 +1,17 @@
+﻿namespace MascotasBackEnd.Models
+{
+    public class Mascota
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Raza { get; set; }
+
+        public string Especie { get; set; }
+
+        public int FechaNacimiento { get; set; }
+        public int IdDueno { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+
+    }
+}
