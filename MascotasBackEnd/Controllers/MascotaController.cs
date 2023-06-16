59476064eb2ae63a0449ad8e7b4d94fc99f66f62
Andres Ochoa <andressochoaa@gmail.com>
2023-06-15 20:24:35 -0500

    Get lista de mascotas mediante MascotaController

--- MascotasBackEnd/Controllers/MascotaController.cs
+++ MascotasBackEnd/Controllers/MascotaController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MascotasBackEnd.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MascotasBackEnd.Controllers
 {
@@ -7,6 +9,28 @@ namespace MascotasBackEnd.Controllers
     [ApiController]
     public class MascotaController : ControllerBase
     {
-        
+        //utilizacion del appdbcontext mediante la inyeccion de dependencia
+        private readonly AppDbContext _context;
+
+        //constructor mascotacontroller
+        public MascotaController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //peticion get
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                //todos los elementos de la tabla mascotas
+                var listMascotas = await _context.Mascotas.ToListAsync();
+                return Ok(listMascotas);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
